c389128ee5ad56a5193b9f49384f63bbc061e8aa
sumitparashar1510 <sparashar.sp27@gmail.com>
2023-03-30 14:05:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
+
+import './App.css';
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
+
+  return (
+    <div className="container mx-4">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto pt-20">No Images Found</h1>}
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto pt-20">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>}
+    </div>
+  );
+}
+
+export default App;
